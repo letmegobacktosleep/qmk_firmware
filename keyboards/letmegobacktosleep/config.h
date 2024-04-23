208681f48165da451e28fe5ca80a7f7fb16fbebe
letmegobacktosleep <167884523+letmegobacktosleep@users.noreply.github.com>
2024-04-23 23:49:24 +1000

    Add files via upload

---
+++ keyboards/letmegobacktosleep/config.h
@@ -0,0 +1,16 @@
+/* Copyright 2023 RephlexZero (@RephlexZero)
+SPDX-License-Identifier: GPL-2.0-or-later */
+#pragma once
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 1
+
+#define DEBUG_ENABLE
+#define DEBUG_MATRIX_SCAN_RATE
+
+#define EECONFIG_KB_DATA_SIZE 6
+
+#define DEBOUNCE 0
+
+#define ADC_RESOLUTION 12
+
+#define CALIBRATION_RANGE 255
