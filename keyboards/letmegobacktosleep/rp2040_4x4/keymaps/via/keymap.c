700ac78f4be1df8b0172348ca94860b524dafcad
letmegobacktosleep <winstonkwan06@gmail.com>
2024-05-28 21:48:03 +1000

    KC_KP_EQUAL didn't seem to work, also changed pin definitions

--- keyboards/letmegobacktosleep/rp2040_4x4/keymaps/via/keymap.c
+++ keyboards/letmegobacktosleep/rp2040_4x4/keymaps/via/keymap.c
@@ -22,7 +22,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_ortho_4x4(
     KC_KP_PLUS,     KC_P7,      KC_P8,      KC_P9,
     KC_KP_MINUS,    KC_P4,      KC_P5,      KC_P6,
-    KC_KP_EQUAL,    KC_P1,      KC_P2,      KC_P3,
+    KC_EQUAL,       KC_P1,      KC_P2,      KC_P3,
     MO(1),          KC_P0,      KC_PDOT,    KC_KP_ENTER),
 
   [1] = LAYOUT_ortho_4x4(
@@ -34,7 +34,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [2] = LAYOUT_ortho_4x4(
     KC_KP_PLUS,     KC_P7,      KC_P8,      KC_P9,
     KC_KP_MINUS,    KC_P4,      KC_P5,      KC_P6,
-    KC_KP_EQUAL,    KC_P1,      KC_P2,      KC_P3,
+    KC_EQUAL,       KC_P1,      KC_P2,      KC_P3,
     MO(3),          KC_P0,      KC_PDOT,    KC_KP_ENTER),
 
   [3] = LAYOUT_ortho_4x4(
